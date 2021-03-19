eba73419123e6540835bd3740224c14976d171b9
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-20 00:30:42 +0100

    created app component and imported on the homepage

---
+++ app/javascript/packs/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const App = () => {
+  return (
+    <div className="welcome-container">
+      <div>
+        <h1>HOME</h1>
+      </div>
+      <div className="footer">
+        <p>2021   |   Jenny</p>
+      </div>
+    </div>
+  )
+}
+export default App;
