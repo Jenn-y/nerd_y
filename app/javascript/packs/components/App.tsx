aea75dd0ed5e5703f919726db3a866b977384ea0
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-24 23:47:32 +0100

    added initial styling

--- app/javascript/packs/components/App.tsx
+++ app/javascript/packs/components/App.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import './css/App.css'
+import Header from './Header'
 
 const App = () => {
   return (
-    <div className="welcome-container">
-      <div>
-        <h1>HOME</h1>
-        <Link to="/posts"><button>Read Posts</button></Link>
-      </div>
-      <div className="footer">
-        <p>2021   |   Jenny</p>
-      </div>
+    <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <Header />
+      <main role="main" className="inner cover">
+        <h1 className="cover-heading">WELCOME.</h1>
+        <p className="lead">"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron"</p>
+        <p className="lead">
+        <Link to="/posts" className="btn btn-lg btn-secondary">Read more</Link>
+        </p>
+      </main>
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
+          <p>Jenn | 2021</p>
+        </div>
+      </footer>
     </div>
   )
 }
--- app/javascript/packs/index.tsx
+++ app/javascript/packs/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App'
 import Posts from './components/Blog/Posts'
+import NewPost from './components/Blog/NewPost'
 import Post from './components/Post/Post'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -14,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <Route exact path="/" component={App} />
     <Route path="/posts" component={Posts} />
     <Route path="/post/:id" component={Post} />
+    <Route path="/newpost" component={NewPost} />
   </Switch>
   </BrowserRouter>
   , document.body.appendChild(document.createElement('div')),
