b97550aa04745b85f05b3009b9c4e1fa51d9af97
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-29 23:23:19 +0200

    completed posts page card view

--- app/javascript/packs/components/Blog/Post.tsx
+++ app/javascript/packs/components/Blog/Post.tsx
@@ -13,7 +13,10 @@ const Post = (post) => {
             <h3 className="card-title">{post.post.title}</h3>
             <button type="button" className="btn btn-outline-warning">{post.post.status}</button>
           </div>
-          <p className="card-text"><ReactMarkdown source={post.post.description} /></p>
+          <p className="card-text">
+            <ReactMarkdown source={post.post.description} />
+            <div className="fadeout"></div>
+          </p>
         </div>
         <div className="card-footer">
           <Link to={`/post/${post.post.id}`}><button type="button" className="btn btn-primary btn-lg btn-block">Read More</button></Link>
