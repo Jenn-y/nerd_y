2331d48bd1be2f6a4814ca9ba6a2d4eeb1810e55
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-31 22:44:07 +0200

    additional post card styling

--- app/javascript/packs/components/Blog/Post.tsx
+++ app/javascript/packs/components/Blog/Post.tsx
@@ -8,17 +8,17 @@ const Post = (post) => {
     <div className="col-lg-4 col-md-6 mb-2-6">
       <article className="card card-style2">
         <img className="card-img-top rounded-top" src={post.post.thumb_image} alt="main image" />
-        <div className="card-body">
+        <div id="custom-card-body">
           <div className="flex-container">
             <h3 className="card-title">{post.post.title}</h3>
-            <button type="button" className="btn btn-outline-warning">{post.post.status}</button>
+            <button type="button" className={"btn btn-outline-" + (post.post.status === 'draft' ? "warning" : "success")}>{post.post.status}</button>
           </div>
           <p className="card-text">
             <ReactMarkdown source={post.post.description} />
             <div className="fadeout"></div>
           </p>
         </div>
-        <div className="card-footer">
+        <div id="custom-card-footer">
           <Link to={`/post/${post.post.id}`}><button type="button" className="btn btn-primary btn-lg btn-block">Read More</button></Link>
         </div>
       </article>
