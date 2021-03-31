dd5af97f2765935bd383316062c831290e3190f3
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-03-31 22:43:22 +0200

    added styling to post forms

--- app/javascript/packs/components/Blog/PostForm.tsx
+++ app/javascript/packs/components/Blog/PostForm.tsx
@@ -3,9 +3,19 @@ import * as React from 'react'
 const PostForm = (post) => {
   return (
     <form onSubmit={post.handleSubmit}>
-      <input onChange={post.handleChange} value={post.post.title} type="text" name="title" placeholder="Title"></input>
-      <input onChange={post.handleChange} value={post.post.description} type="text" name="description" placeholder="Write your post here..."></input>
-      <button type="submit">Submit</button>
+      <div className="form-group">
+        <label htmlFor="exampleFormControlInput1">Title:</label>
+        <input onChange={post.handleChange} value={post.post.title} 
+          type="text" name="title" placeholder="Title" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleFormControlTextarea1">Content:</label>
+        <textarea className="form-control" rows={10}
+          onChange={post.handleChange} value={post.post.description}
+          name="description" placeholder="Write your post here...">
+        </textarea>
+      </div>
+      <button type="submit" onClick={post.handleReturn} className="btn btn-success btn-lg btn-block">Submit</button>
     </form>
   )
 }
--- app/javascript/packs/components/Post/PostForm.tsx
+++ app/javascript/packs/components/Post/PostForm.tsx
@@ -3,10 +3,20 @@ import * as React from 'react'
 const PostForm = (post) => {
   return (
     <form onSubmit={post.handleSubmit}>
-      <input onChange={post.handleChange} value={post.post.title} type="text" name="title" placeholder="Title"></input>
-      <input onChange={post.handleChange} value={post.post.description} type="text" name="description" placeholder="Write your post here..."></input>
-      <button type="submit" onClick={() => post.changeStatus(0)}>Save as a draft</button>
-      <button type="submit" onClick={() => post.changeStatus(1)}>Publish</button>
+      <div className="form-group">
+        <label htmlFor="exampleFormControlInput1">Title:</label>
+        <input onChange={post.handleChange} value={post.post.title} 
+          type="text" name="title" placeholder="Title" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleFormControlTextarea1">Content:</label>
+        <textarea className="form-control" rows={10}
+          onChange={post.handleChange} value={post.post.description}
+          name="description" placeholder="Write your post here...">
+        </textarea>
+      </div>
+      <button type="submit" onClick={() => post.changeStatus("draft")} className="btn btn-danger">Save as a draft</button>
+      <button type="submit" onClick={() => post.changeStatus("published")} className="btn btn-success">Publish</button>
     </form>
   )
 }
